7ab78d88be8be99e38bc8a4725a4a2e9b10a0480
KyunHwan <kyunhwan.peter@gmail.com>
2026-02-15 20:23:09 +0900

    fixed versioning

--- transformer_lib_patch.sh
+++ transformer_lib_patch.sh
@@ -1,1 +1,3 @@
-cp -r ./trainer/policy_constructor/model_constructor/blocks/experiments/third_party/openpi/models_pytorch/transformers_replace/* .venv/lib/python3.11/site-packages/transformers/
+#!/bin/bash
+
+cp -r ./trainer/policy_constructor/model_constructor/blocks/experiments/third_party/openpi/models_pytorch/transformers_replace/* .venv/lib/python3.12/site-packages/transformers/
