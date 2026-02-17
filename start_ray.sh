6de6da76ebfc1f2738c815344d19834b51ef42ee
KyunHwan <kyunhwan.peter@gmail.com>
2026-02-17 21:34:42 +0900

    updated trainer for openpi with bash file for starting ray

---
+++ start_ray.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# start_ray.sh — run on each machine
+
+HEAD_IP="192.168.0.134"
+HOSTNAME=$(hostname)
+
+case "$HOSTNAME" in
+  robros-ai1)
+    ray start --head --port=6379 \
+      --resources='{"training_pc": 3}'
+    ;;
+  robros-MS-7E59)
+    ray start --address=${HEAD_IP}:6379 \
+      --resources='{"labeling_pc": 1}'
+    ;;
+  robros-5090)
+    ray start --address=${HEAD_IP}:6379 \
+      --resources='{"inference_pc": 1}'
+    ;;
+  *)
+    echo "Unknown host: $HOSTNAME"
+    exit 1
+    ;;
+esac
